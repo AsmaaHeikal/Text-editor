49fa610d1fde9c59fe5b5fecb379cb966db9538f
AsmaaHeikal <AsmaaIHeikal@gmail.com>
2022-05-16 16:15:30 +0200

    Added the project files

---
+++ text editor (1).h
@@ -0,0 +1,27 @@
+
+
+#ifndef TEXT_EDITOR__CS112_203_2ND_S1_20210049_20210067_20210133_A4_TEXTEDITORFUNC_H
+#define TEXT_EDITOR__CS112_203_2ND_S1_20210049_20210067_20210133_A4_TEXTEDITORFUNC_H
+#include<fstream>
+using namespace std;
+char fileName[81];
+fstream file;
+void countWord( fstream & file);
+void appendText(fstream &file);
+void Displaycontent(fstream&file);
+void empty_file(fstream&file);
+void encrypt(fstream&file);
+
+void merge();
+void count_words();
+void count_chars();
+void count_lines();
+void search_word();
+
+void decrypt(fstream&file);
+void upperCase(fstream &file);
+void lowerCase(fstream &file);
+void firstIsUpper (fstream & file);
+void save();
+
+#endif //TEXT_EDITOR__CS112_203_2ND_S1_20210049_20210067_20210133_A4_TEXTEDITORFUNC_H
